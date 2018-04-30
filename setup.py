bdc610b9a8081f5030e2f15a2da295a667dcd391
Maik Messerschmidt <maik.messerschmidt@gmx.net>
2018-04-30 12:58:16 +0200

    Added files for distributing sudokutools.

---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+import sudokutools
+
+version = sudokutools.__version__
+description = sudokutools.__doc__.split('\n')[0]
+long_description = sudokutools.__doc__
+
+setup(
+    name = 'sudokutools',
+    packages = ['sudokutools'],
+    version = version,
+    description = description,
+    long_description=long_description,
+    author = sudokutools.__author__,
+    author_email = sudokutools.__email__,
+    license = sudokutools.__license__,
+    url = 'https://github.com/messersm/sudokutools',
+    download_url = 'https://github.com/messersm/sudokutools/tarball/%s' % version,
+    keywords = ['sudokutools'],
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
